f3c8c42e159fd00749d9d4f36bfd7fa12f88fa00
Alex168 <sashok1356@gmail.com>
2021-02-08 22:22:43 +0300

    Ability to edit user profile

--- app/src/main/java/com/example/game/MainActivity.java
+++ app/src/main/java/com/example/game/MainActivity.java
@@ -1,11 +1,23 @@
 package com.example.game;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.game.Models.User;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.Collections;
 import java.util.List;
 
@@ -13,6 +25,11 @@ public class MainActivity extends AppCompatActivity {
     private static final int RC_SIGN_IN = 1;
     private FirebaseAuth mAuth;
     private static final String TAG = "MainActivity";
+    private Button mLogOutButton;
+    private Button mNewGameButton;
+    private Button mJoinGameButton;
+    private Button mProfileButton;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +37,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference("users");
+        mJoinGameButton = findViewById(R.id.join_game_button);
+        mNewGameButton = findViewById(R.id.new_game_button);
+        mLogOutButton = findViewById(R.id.log_out_button);
+        mProfileButton = findViewById(R.id.profile_button);
 
+        mLogOutButton.setOnClickListener(v -> {
+            mAuth.signOut();
+            recreate();
+        });
+
+        mProfileButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+            startActivity(intent);
+        });
     }
 
     @Override
@@ -48,14 +79,31 @@ public class MainActivity extends AppCompatActivity {
 
             if (resultCode == RESULT_OK) {
                 // Successfully signed in
-                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                // ...
+                createUser();
             } else {
-                // Sign in failed. If response is null the user canceled the
-                // sign-in flow using the back button. Otherwise check
-                // response.getError().getErrorCode() and handle the error.
-                // ...
+                Toast.makeText(MainActivity.this, R.string.failedLoginMsg, Toast.LENGTH_SHORT).show();
+                // Sign in failed
             }
         }
+    }
+
+    private void createUser() {
+        // create game user if doesn't exist yet
+        String key = mAuth.getCurrentUser().getUid();
+        mDatabase.child(key).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User dbUser = snapshot.getValue(User.class);
+                if (dbUser == null) {
+                    User user = new User();
+                    mDatabase.child(key).setValue(user);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/game/ProfileActivity.java
@@ -0,0 +1,185 @@
+package com.example.game;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.game.Models.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+public class ProfileActivity extends AppCompatActivity {
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private FirebaseAuth mAuth;
+    private Button mSaveButton;
+    private ImageButton mPickImage;
+    private TextView mGamesPlayed;
+    private TextView mGamesWon;
+    private TextView mWinRate;
+    private ImageView mImageView;
+    private EditText mNickname;
+
+    private Uri mImageUri;
+    private DatabaseReference mDatabase;
+    private StorageTask mUploadTask;
+    private StorageReference mStorage;
+    private boolean isUploaded;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        mSaveButton = findViewById(R.id.saveButton);
+        mPickImage = findViewById(R.id.pickImageButton);
+        mGamesPlayed = findViewById(R.id.gamesPlayedValueTextView);
+        mGamesWon = findViewById(R.id.gamesWonValueTextView);
+        mWinRate = findViewById(R.id.winRateValueTextView);
+        mImageView = findViewById(R.id.imageView);
+        mNickname = findViewById(R.id.editNickname);
+        mGamesPlayed.setText("0");
+        mGamesWon.setText("0");
+        mWinRate.setText("100 %");
+
+        isUploaded = false;
+
+        mStorage = FirebaseStorage.getInstance().getReference("uploads");
+        mDatabase = FirebaseDatabase.getInstance().getReference("users");
+        mAuth = FirebaseAuth.getInstance();
+
+        subscribe();
+
+        mSaveButton.setOnClickListener(v -> {
+            if (saveImage() && saveNickname()) { finish(); }
+        });
+
+        mPickImage.setOnClickListener(v -> chooseImage());
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null) {
+            mImageUri = data.getData();
+            Picasso.get().load(mImageUri).into(mImageView);
+        }
+    }
+
+    private void subscribe(){
+        String key = mAuth.getCurrentUser().getUid();
+        mDatabase.child(key).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User user = snapshot.getValue(User.class);
+                if (user == null) {
+                    return;
+                }
+                String nickname = user.nickname;
+                if (nickname != null) {
+                    mNickname.setText(nickname);
+                }
+                Integer gamesPlayed = user.gamesPlayed;
+                Integer gamesWon = user.gamesWon;
+                if (gamesPlayed == null){
+                    return;
+                }
+                mGamesPlayed.setText(gamesPlayed.toString());
+                if (gamesWon != null) {
+                    mGamesWon.setText(gamesWon.toString());
+                    if (gamesWon != 0) {
+                        String winRate = Double.toString(Math.floor((double)gamesWon / (double)gamesPlayed * 10000)/100).concat(" %");
+                        mWinRate.setText(winRate);
+                    }
+                }
+                String imagePath = user.imagePath;
+
+                if (imagePath != null && !imagePath.isEmpty()) {
+                    Picasso.get().load(imagePath).into(mImageView);
+                    isUploaded = true;
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void chooseImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    private boolean saveImage() {
+        if (mImageUri != null) {
+            StorageReference fileReference = mStorage.child(System.currentTimeMillis()
+                    + "." + getFileExtension(mImageUri));
+
+            mUploadTask = fileReference.putFile(mImageUri)
+                    .addOnSuccessListener(taskSnapshot -> taskSnapshot.getStorage().getDownloadUrl().addOnCompleteListener(task -> {
+                        String upload = task.getResult().toString();
+                        mDatabase.child(mAuth.getUid()).child("imagePath").setValue(upload);
+                    }))
+                    .addOnFailureListener(e -> Toast.makeText(ProfileActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show());
+            return true;
+        }
+        else if (isUploaded) {
+            return true;
+        }
+        else {
+            Toast.makeText(this, R.string.imageNotChosen, Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+    private boolean saveNickname() {
+        String newNickname = mNickname.getText().toString();
+        if (newNickname.equals("")) {
+            Toast.makeText(this, R.string.emptyNickname, Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        mDatabase.child(mAuth.getUid()).child("nickname").setValue(newNickname);
+        return true;
+    }
+
+    private String getFileExtension(Uri uri) {
+        ContentResolver cR = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cR.getType(uri));
+    }
+}
